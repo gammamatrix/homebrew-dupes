224d88f8e1f206dbcf0389915f91497d09a941cf
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2014-01-18 14:23:07 -0800

    Adds the option: --with-ldap - Include support for LDAP
    To see the LDAP option the command:
    
    `brew options httpd`
    
    This patch makes it easier to add more options by using:
    
    ```
    if build.with?
    ```

--- httpd.rb
+++ httpd.rb
@@ -7,29 +7,48 @@ class Httpd < Formula
 
   skip_clean :la
 
+  option "with-ldap", "Include support for LDAP"
+
+  def install_args
+    args = [
+      "--prefix=#{prefix}",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--mandir=#{man}",
+      "--localstatedir=#{var}/apache2",
+      "--sysconfdir=#{etc}/apache2",
+      "--enable-layout=GNU",
+      "--enable-mods-shared=all",
+      "--with-ssl=/usr",
+      "--with-mpm=prefork",
+      "--disable-unique-id",
+      "--enable-ssl",
+      "--enable-dav",
+      "--enable-cache",
+      "--enable-proxy",
+      "--enable-logio",
+      "--enable-deflate",
+      "--with-included-apr",
+      "--enable-cgi",
+      "--enable-cgid",
+      "--enable-suexec",
+      "--enable-rewrite",
+    ]
+
+    if build.with? 'ldap'
+      args << "--with-ldap"
+      args << "--enable-ldap"
+      args << "--enable-authnz-ldap"
+    end
+
+    args
+  end
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--localstatedir=#{var}/apache2",
-                          "--sysconfdir=#{etc}/apache2",
-                          "--enable-layout=GNU",
-                          "--enable-mods-shared=all",
-                          "--with-ssl=/usr",
-                          "--with-mpm=prefork",
-                          "--disable-unique-id",
-                          "--enable-ssl",
-                          "--enable-dav",
-                          "--enable-cache",
-                          "--enable-proxy",
-                          "--enable-logio",
-                          "--enable-deflate",
-                          "--with-included-apr",
-                          "--enable-cgi",
-                          "--enable-cgid",
-                          "--enable-suexec",
-                          "--enable-rewrite"
+    args = install_args
+
+    system "./configure", *args
+
     system "make"
     system "make install"
     (var/'apache2/log').mkpath
